6536822fed8a49f5d035d0404733e4843ba7afd1
larssonmaxeng <larsson.maxeng@gmail.com>
2022-12-17 10:56:23 -0300

    T

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from app import materiaisPedidos
 from os.path import abspath, dirname, join
 app = Flask(__name__)
-conexao = "sqlite:///fuzzyController.sqlite"
+conexao =  "sqlite:///fuzzyController.sqlite"
 BASE_DIR = dirname(dirname(abspath(__file__)))
 SHAPE_DIR = join(BASE_DIR,"app","static","arquivos")
 app.config['SECRET_KEY'] = 'q1c2v3b4$$&&'
@@ -13,5 +13,5 @@ app.config['SQLALCHEMY_TRACKMODIFICATIONS'] = False
 app.config["UPLOAD_FOLDER"] = SHAPE_DIR
 app.register_blueprint(materiaisPedidos.bp_materialPedidos, url_prefix = '/materiaisPedidos')
 database.db.init_app(app)
-migrate= Migrate(app, database.db)
+#migrate= Migrate(app, database.db)
 from app import routes
